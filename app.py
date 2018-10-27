3fbc736a46ac4d8080b25f63067fdb753be80a5f
Hans_Tausend <einund.tausend@gmail.com>
2018-10-27 20:16:08 +0200

    simple return query

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ def index():
     return jsonify({'message': 'It works!'})
 
 
+# validation of params for parameters
+@app.route('/fill-wallet-for-gas/', methods=['GET'])
+def returnQuery():
+    gasNeeded = request.args.get('gas_needed')
+    address = requestself.args.get('public_address')
+    speedNeeded = request.args.get('tx_speed')
+    return jsonify({"gas requested": gasNeeded}, {"public address": address}, {"speed requested": speedNeeded})
+
+
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({"error": "page not found"})
