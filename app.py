c63bdea140b618a25bb495dc3aaa477dda6ef17a
Hans_Tausend <einund.tausend@gmail.com>
2018-10-27 20:13:00 +0200

    added 404 error handler

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-
 def index():
     return jsonify({'message': 'It works!'})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "page not found"})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
