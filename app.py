b6d1e533998777d55dc2b247a4bfd5119a383b29
Hans_Tausend <einund.tausend@gmail.com>
2018-10-27 23:11:52 +0200

    additional error handling

--- app.py
+++ app.py
@@ -1,26 +1,65 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from calcEthNeeded import calcEthNeeded
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def index():
-    return jsonify({'message': 'It works!'})
+    return jsonify({'message': 'It works!'},{"example request": "http://localhost:8000/fill-wallet-for-gas?gas_needed=10000&tx_speed=medium&public_address=0x516F329EC1fF7BF6882dE762A14eb94491FA4D8d"})
 
-
-# validation of params for parameters
+#handeling parameters
 @app.route('/fill-wallet-for-gas/', methods=['GET'])
 def returnQuery():
-    gasNeeded = request.args.get('gas_needed')
-    print(gasNeeded)
-    address = request.args.get('public_address')
-    speedNeeded = request.args.get('tx_speed')
-    ethNeeded = calcEthNeeded(gasNeeded,speedNeeded)
-    return jsonify({"gas requested": gasNeeded}, {"public address": address}, {"speed requested": speedNeeded}, {"ethNeeded in Wei": ethNeeded })
+    try:
+        #handeling invalid request parameter
+        if not request.args.get('gas_needed'):
+            raise InvalidUsage('gas_needed property incorrect', status_code=500)
+        if not request.args.get('public_address'):
+            raise InvalidUsage('public_address  property incorrect', status_code=500)
+        if not request.args.get('tx_speed'):
+            raise InvalidUsage('tx_speed property incorrect', status_code=500)
 
+        gasNeeded = int(request.args.get('gas_needed'))
+        address = request.args.get('public_address')
+        speedNeeded = request.args.get('tx_speed')
 
+        ethNeeded = calcEthNeeded(gasNeeded, speedNeeded)
+        return jsonify({"gas requested": gasNeeded}, {"public address": address}, {"speed requested": speedNeeded}, {"ethNeeded in Wei": ethNeeded})
+    #handleing exceptions
+    except Exception:
+        raise InvalidUsage('Invalid input', status_code=500)
+
+#handeling invalid routes
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({"error": "page not found"})
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({"error": "internal server error"})
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
 
 
 if __name__ == '__main__':
--- calcEthNeeded.py
+++ calcEthNeeded.py
@@ -1,8 +1,7 @@
 def calcEthNeeded(gasNeeded, speed):
-    gas = int(gasNeeded)
     if speed == 'fast':
-        return (40 * gas) /  (10 ** 9)
+        return (40 * gasNeeded) /  (10 ** 9)
     if speed =='medium':
-        return (4 * gas) /   (10 ** 9)
+        return (4 * gasNeeded) /   (10 ** 9)
     if speed == 'slow':
-        return (3 * gas) /   (10 ** 9)
+        return (3 * gasNeeded) /   (10 ** 9)
