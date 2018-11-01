cc8a5c7d404a4d36fa060bce5c9d60a0be74aa92
root <einund.tausend@gmail.com>
2018-11-01 17:16:18 +0100

    added testing

--- calcEthNeeded.py
+++ calcEthNeeded.py
@@ -5,39 +5,66 @@ from web3.auto import w3
 from web3.middleware import geth_poa_middleware
 from web3.gas_strategies.time_based import fast_gas_price_strategy, slow_gas_price_strategy,medium_gas_price_strategy
 from web3.gas_strategies.rpc import rpc_gas_price_strategy
-from cachetools import TTLCache, cachedmethod
+from web3.middleware.cache import construct_simple_cache_middleware
+from cachetools import TTLCache, cachedmethod, cached, LRUCache
 from functools import partial
-from cachetools import cached, LRUCache
 from cachetools.keys import hashkey
+import functools
 import threading
+import cachetools.func
+
 
 #import timeit
 #start = timeit.default_timer()
 
 w3.eth.enable_unaudited_features()
 token = os.environ['INFURA_TOKEN']
-url = "https://ropsten.infura.io/%s" % token
+url = "https://mainnet.infura.io/%s" % token
 w3 = Web3(Web3.HTTPProvider(url))
 #w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 
 w3.eth.setGasPriceStrategy(fast_gas_price_strategy)
 #setting ttl to < 0.000001 will show the effect
-cache = TTLCache(maxsize=10000, ttl=60)
+#cache = TTLCache(maxsize=10000, ttl=60)
+#cache = LRUCache(maxsize=10000)
 
 
-@cached(cache, key=partial(hashkey, 'gas'))
+def testBool(method,params,response):
+        return True
+
+block_hash_cache_middleware = construct_simple_cache_middleware(
+       # default sample size of gas price strategies is 120
+       cache_class=functools.partial(LRUCache, 150),
+       rpc_whitelist='eth_getBlockByHash',
+       should_cache_fn=testBool
+)
+
+w3.middleware_stack.add(block_hash_cache_middleware)
+
+
+
+
+
+
+
+
+
+#@cached(cache, key=partial(hashkey, 'gas'))
 def calcEthNeeded(gasNeeded, speed):
    if speed == 'fast':
        w3.eth.setGasPriceStrategy(fast_gas_price_strategy)
        gasPrice = w3.eth.generateGasPrice()
+       print(gasPrice)
        return (gasPrice * gasNeeded) /  (10 ** 9)
    if speed =='medium':
        w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
        gasPrice = w3.eth.generateGasPrice()
+       print(gasPrice)
        return (gasPrice * gasNeeded) /  (10 ** 9)
    if speed == 'slow':
        w3.eth.setGasPriceStrategy(slow_gas_price_strategy)
        gasPrice = w3.eth.generateGasPrice()
+       print(gasPrice)
        return (gasPrice * gasNeeded) /  (10 ** 9)
 
 
@@ -52,7 +79,9 @@ def set_interval(func, sec):
     t.start()
     return t
 
-set_interval(calcEthNeeded,10)
+#initializing
+#calcEthNeeded(1000,'fast')
+#set_interval(calcEthNeeded,10)
 
 
 
---
+++ testScript.py
@@ -0,0 +1,43 @@
+import requests
+import timeit
+import threading
+
+payload = {'gas_needed': '10000', 'tx_speed': 'medium', 'public_address': '0x516F329EC1fF7BF6882dE762A14eb94491FA4D8d'}
+
+for i in range(10000):
+    start = timeit.default_timer()
+    r = requests.get('http://localhost:8000/fill-wallet-for-gas', params=payload)
+    #print(r.json())
+    stop = timeit.default_timer()
+    print('Time: ', stop - start)
+
+
+
+def blockreq():
+    start = timeit.default_timer()
+    r = requests.get('http://localhost:8000/fill-wallet-for-gas', params=payload)
+    stop = timeit.default_timer()
+    print('Time: ', stop - start)
+
+
+
+
+
+
+i = 1
+
+def set_interval(func, sec):
+    def func_wrapper():
+        #++sec
+        #print(sec)
+        set_interval(func, sec)
+        func()
+    #++sec
+    #print(sec)
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+#set_interval(blockreq,1)
+
+
