fa6a372b110804e34221ff9165e79264fa05c6c2
root <einund.tausend@gmail.com>
2018-11-09 02:36:50 +0100

    errors simplified

--- app.py
+++ app.py
@@ -95,45 +95,15 @@ def returnQuery():
         message = template.format(type(ex).__name__, ex.args)
         return jsonify(success=False, errors=message)
 
-
-
-
-
-
-
-#handeling invalid routes
+#Handling invalid routes
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({"error": "page not found"})
-
-#handeling internal server errors
+#Handling internal server errors
 @app.errorhandler(500)
 def internal_server_error(error):
     return jsonify({"error": "internal server error"})
-
-#custom Error Code
-class InvalidUsage(Exception):
-    status_code = 400
-
-    def __init__(self, message, status_code=None, payload=None):
-        Exception.__init__(self)
-        self.message = message
-        if status_code is not None:
-            self.status_code = status_code
-        self.payload = payload
-
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv['message'] = self.message
-        return rv
-
-@app.errorhandler(InvalidUsage)
-def handle_invalid_usage(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
-
-
-
+#Running the app 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
+
